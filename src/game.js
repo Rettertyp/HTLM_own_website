448074795b299fe3f47f19ec18c1b00d0c0d3021
Marius Kaufmann <marius@kaufmanns.de>
2021-07-05 17:25:31 +0200

    trying to load the json file

--- src/game.js
+++ src/game.js
@@ -27,9 +27,9 @@ request.send();
 
 let GAMESTATE;
 // when the request has been granted and the data has been loaded, do this:
-GAMESTATE = request.onload = function() {
+request.onload = function() {
   // save the data from the json file into the "GAMESTATE"-Object
-  return request.response;
+  console.log(request.response);
 }
 
 
