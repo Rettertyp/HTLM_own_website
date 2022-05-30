45de023d4517bdb33066af0631e2e02c4dde6115
Rettertyp <marius@kaufmanns.de>
2022-05-30 19:13:27 +0200

    add keyboard + controller input simultaneously

--- src/input.js
+++ src/input.js
@@ -11,11 +11,56 @@ export default class InputHandler {
     window.addEventListener("gamepadconnected", (e) => {
       this.gamepad = navigator.getGamepads()[e.gamepad.index];
     });
+
+    // adding event listeners in order to be able to also control it via the keyboard
+    document.addEventListener("keydown", (event) => {
+      switch (event.keyCode) {
+        // when the left arrow is down => keyCode = 37, we tell the paddle to move left
+        case 37:
+          game.paddle.moveLeft();
+          break;
+        // when the right arrow is down => keyCode = 39, we tell the paddle to move right
+        case 39:
+          game.paddle.moveRight();
+          break;
+
+        // keyCode 27 = escape key
+        case 27:
+          game.togglePause();
+          break;
+
+        // keyCode 32 = space bar
+        case 32:
+          game.start();
+          break;
+
+        default:
+          break;
+      }
+    });
+
+    // another Event listener, that tracks whether the keys are NOT pressed
+    document.addEventListener("keyup", (event) => {
+      switch (event.keyCode) {
+        // when the left arrow is down => keyCode = 37, we tell the paddle to move left
+        case 37:
+          // stop in case we're travelling left at the moment, else dont
+          if (game.paddle.speed < 0) game.paddle.stop();
+          break;
+        // when the right arrow is down => keyCode = 39, we tell the paddle to move right
+        case 39:
+          // stop in case we're travelling left at the moment, else dont
+          if (game.paddle.speed > 0) game.paddle.stop();
+          break;
+
+        default:
+          break;
+      }
+    });
   }
 
   // fetch all inputs from the gamepad and react to them
   update(deltaTime) {
-
     // return when the gamepad isn't connected (yet)
     if (!this.gamepad) {
       return;
